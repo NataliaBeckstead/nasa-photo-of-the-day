920e515a25993103bf76ca13892eb0269c98c953
Natalia Beckstead <toosmiravilla@gmail.com>
2020-02-19 16:07:22 -0700

    main task finished

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Photo from "./Components/MainSection/Content";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Photo/>
       <Footer/>
     </div>
   );
---
+++ src/Components/MainSection/Content.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Photo() {
+    const [photo, setPhoto] = useState([]);
+  
+    useEffect(() => {
+      axios
+        .get(`https://api.nasa.gov/planetary/apod?api_key=2CuqxZAJGsqU820I1TaRLIyaBYlhVaW4p9woRwx3`)
+        .then(response => {
+          console.log(response.data);
+          setPhoto(response.data);
+        })
+        .catch(error => {
+          console.log("the data was not return", error);
+        });
+    }, []);
+    return (
+        <div className="main-container">
+            <div className="date-descriptoion">
+                <h3>{photo.date}</h3><p className="explanation">{photo.explanation}</p>
+            </div>
+            <div className="day-photocont">
+                <img className="day-photo" src={photo.hdurl} alt="NASA photo of the day"/>
+            </div>
+            
+        {/* {films.map(film => {
+          return (
+            <MovieCard
+              key={film.id}
+              title={film.title}
+              description={film.description}
+              director={film.director}
+              release_date={film.release_date}
+            />
+          );
+        })} */}
+        </div>
+    );
+  }
+  
