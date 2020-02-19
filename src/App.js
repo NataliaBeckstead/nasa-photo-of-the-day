d1a2e9a958ace5d0d200f96830336231a7585c48
Natalia Beckstead <toosmiravilla@gmail.com>
2020-02-19 15:09:30 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
+//import banana from "./Components/Header/Header.js";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => { 
+    return (
+        <div className="App-header">
+            <div>
+                <img className="App-logo" src="https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png" alt="NASA logo" />
+                <h1>Astronomy Picture of the Day</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
