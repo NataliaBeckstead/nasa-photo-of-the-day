d51f2bc50405d382aefbbd0b6d73ac9262956688
Natalia Beckstead <toosmiravilla@gmail.com>
2020-02-20 14:58:06 -0700

    main task finished

--- src/Components/MainSection/Content.js
+++ src/Components/MainSection/Content.js
@@ -2,6 +2,58 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import styled from "styled-components";
+
+const DayPhoto = styled.img `
+  max-width: 100%;
+  height: auto;
+  border-radius: 12px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
+
+const PhotoCont  = styled.div `
+  max-width: 100%;
+  width: 80%;
+`;
+
+const Explanation = styled.p `
+  width: 50%;
+  margin: 2rem;
+  justify-content: flex-start;
+  text-align: left;
+`;
+
+const DateText = styled.h3 `
+  font-size: 2.5rem;
+  text-align: center;
+  align-self: center;
+`;
+
+const DateContainer = styled.div`
+  width: 50%;
+  font-size: 2.5rem;
+  text-align: center;
+  align-self: center;
+`;
+
+const DateDescriptoion = styled.div `
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  margin-bottom: 2rem;
+`;
+
+const MainContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width:100%;
+  max-width: 100%;
+  justify-content: center;
+  margin-top: 2rem;
+  margin-bottom: 4rem;
+`;
+
 
 export default function Photo() {
     const [photo, setPhoto] = useState([]);
@@ -54,23 +106,23 @@ export default function Photo() {
     const newPhoto = CallNewContent(calanderDate);
 
     return (
-        <div className="main-container">
-            <div className="date-descriptoion">
-                <div className="date-container">
-                    <h3>{useCalendar ? photo.date : newPhoto.date}</h3>
+        <MainContainer>
+            <DateDescriptoion>
+                <DateContainer>
+                    <DateText>{useCalendar ? photo.date : newPhoto.date}</DateText>
                     <DatePicker 
                        selected={startDate}
                        onChange={handleDateChange}
                        placeholderText="choose a date" 
                     />
-                    <p className="choose-text">Choose a day</p>
-                </div>  
-                <p className="explanation">{useCalendar ? photo.explanation : newPhoto.explanation}</p>
-            </div>
-            <div className="day-photocont">
-                <img className="day-photo" src={useCalendar ? photo.hdurl : newPhoto.hdurl} alt="NASA photo of the day"/>
-            </div>
-        </div>
+                    <p>Choose a day</p>
+                </DateContainer>  
+                <Explanation>{useCalendar ? photo.explanation : newPhoto.explanation}</Explanation>
+            </DateDescriptoion>
+            <PhotoCont>
+                <DayPhoto src={useCalendar ? photo.hdurl : newPhoto.hdurl} alt="NASA photo of the day"/>
+            </PhotoCont>
+        </MainContainer>
     );
   }
   
